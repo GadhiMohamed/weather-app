b0ad19ce6cbbdb93961b78fc58f891fdbe140dfb
GadhiMohamed <mohamedgadhi91@gmail.com>
2018-11-11 14:33:27 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -24,26 +24,37 @@ class App extends React.Component {
         const country = e.target.elements.country.value;
         const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
-        if (data.cod === '404') {
-            this.setState({
-                error: 'City not found'
-            });
-        } else if (city && country) {
+        if (city && country && data.cod !== "404") {
+            console.log(data);
             this.setState({
                 temperature: data.main.temp,
                 city: data.name,
                 country: data.sys.country,
                 humidity: data.main.humidity,
                 description: data.weather[0].description,
-                icon: data.weather[0].icon,
-                error: null,
+                error: ""
+            });
+        } else if (city && country && data.message === "city not found" && data.cod === "404") {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "City not found"
             });
         } else {
             this.setState({
-                error: 'Nothing to geocode'
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter values"
             });
         }
-    };
+    }
+
 
     render() {
         return (
