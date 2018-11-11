97077424a1317fd374f172c49168d8158e5ab441
GadhiMohamed <mohamedgadhi91@gmail.com>
2018-11-11 13:11:07 +0000

    Updates

---
+++ precache-manifest.2585694a04ab2a35391732c5c66ffbdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5afd6fae19e662aefcb7e269474c56c",
+    "url": "/weather-app/static/media/bg.b5afd6fa.jpg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "eb6d7ba0c046abe4acd2",
+    "url": "/weather-app/static/js/main.eb6d7ba0.chunk.js"
+  },
+  {
+    "revision": "573ef2075ecf2f56ef3a",
+    "url": "/weather-app/static/js/1.573ef207.chunk.js"
+  },
+  {
+    "revision": "eb6d7ba0c046abe4acd2",
+    "url": "/weather-app/static/css/main.1eb02c36.chunk.css"
+  },
+  {
+    "revision": "573ef2075ecf2f56ef3a",
+    "url": "/weather-app/static/css/1.ac02096b.chunk.css"
+  },
+  {
+    "revision": "67bf226e15349a03ec036fd1c409e8bf",
+    "url": "/weather-app/index.html"
+  }
+];
